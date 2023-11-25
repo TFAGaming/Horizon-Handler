a9cb4545b73b6c852403b112f8e344767ee9dd9d
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2023-11-25 18:17:38 +0100

    add useMatch to componet Builder

--- src/core/ComponentBuilder.ts
+++ src/core/ComponentBuilder.ts
@@ -7,12 +7,14 @@ export class ComponentBuilder<C extends Client> {
     private readonly disabled?: ComponentStructure<C>['disabled'];
     public readonly type: ComponentStructure<C>['type'];
     public readonly customId: ComponentStructure<C>['customId'];
+    public readonly useMatch?: ComponentStructure<C>["useMatch"];
     public readonly run: ComponentStructure<C>['run'];
     
     constructor(data: ComponentStructure<C>) {
         this.type = data.type;
         this.customId = data.customId;
         this.run = data.run;
+        this.useMatch = data.useMatch;
         this.disabled = data.disabled;
     };
 
@@ -21,6 +23,7 @@ export class ComponentBuilder<C extends Client> {
             type: this.type,
             customId: this.customId,
             run: this.run,
+            useMatch:this.useMatch,
             disabled: this.disabled
         } as ComponentStructure<C>;
     };
